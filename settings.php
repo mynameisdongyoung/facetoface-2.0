1e62fbc4b2481eb18c920c155579035f33556e29
Dongyoung Dennis Kim <dckyoung@gmail.com>
2012-07-16 14:34:24 -0500

    Update master

--- settings.php
+++ settings.php
@@ -1,292 +1,57 @@
 <?php
-global $DB, $OUTPUT, $PAGE;
+global $CFG, $USER,$DB;
+require_once "$CFG->dirroot/mod/facetoface/lib.php";
 
-require_once '../../config.php';
-require_once 'lib.php';
-require_once 'session_form.php';
+$settings->add(new admin_setting_configtext('facetoface_fromaddress', get_string('setting:fromaddress_caption', 'facetoface'),get_string('setting:fromaddress', 'facetoface'), get_string('setting:fromaddressdefault', 'facetoface'), "/^((?:[\w\.\-])+\@(?:(?:[a-zA-Z\d\-])+\.)+(?:[a-zA-Z\d]{2,4}))$/",30));
 
-$id = optional_param('id', 0, PARAM_INT); // Course Module ID
-$f = optional_param('f', 0, PARAM_INT); // facetoface Module ID
-$s = optional_param('s', 0, PARAM_INT); // facetoface session ID
-$c = optional_param('c', 0, PARAM_INT); // copy session
-$d = optional_param('d', 0, PARAM_INT); // delete session
-$confirm = optional_param('confirm', false, PARAM_BOOL); // delete confirmation
-
-$nbdays = 1; // default number to show
-
-$session = null;
-if ($id) {
-    if (!$cm = $DB->get_record('course_modules', array('id' => $id))) {
-        print_error('error:incorrectcoursemoduleid', 'facetoface');
-    }
-    if (!$course = $DB->get_record('course', array('id' => $cm->course))) {
-        print_error('error:coursemisconfigured', 'facetoface');
-    }
-    if (!$facetoface =$DB->get_record('facetoface',array('id' => $cm->instance))) {
-        print_error('error:incorrectcoursemodule', 'facetoface');
-    }
-}
-elseif ($s) {
-     if (!$session = facetoface_get_session($s)) {
-         print_error('error:incorrectcoursemodulesession', 'facetoface');
-     }
-     if (!$facetoface = $DB->get_record('facetoface',array('id' => $session->facetoface))) {
-         print_error('error:incorrectfacetofaceid', 'facetoface');
-     }
-     if (!$course = $DB->get_record('course', array('id'=> $facetoface->course))) {
-         print_error('error:coursemisconfigured', 'facetoface');
-     }
-     if (!$cm = get_coursemodule_from_instance('facetoface', $facetoface->id, $course->id)) {
-         print_error('error:incorrectcoursemoduleid', 'facetoface');
-     }
-
-     $nbdays = count($session->sessiondates);
-}
-else {
-    if (!$facetoface = $DB->get_record('facetoface', array('id' => $f))) {
-        print_error('error:incorrectfacetofaceid', 'facetoface');
-    }
-    if (!$course = $DB->get_record('course', array('id' => $facetoface->course))) {
-        print_error('error:coursemisconfigured', 'facetoface');
-    }
-    if (!$cm = get_coursemodule_from_instance('facetoface', $facetoface->id, $course->id)) {
-        print_error('error:incorrectcoursemoduleid', 'facetoface');
+// Load roles
+$choices = array();
+if ($roles = $DB->get_records('role')) {
+    foreach ($roles as $role) {
+        $choices[$role->id] = format_string($role->name);
     }
 }
 
-require_course_login($course);
-$errorstr = '';
-$context = context_course::instance($course->id);
-require_capability('mod/facetoface:editsessions', $context);
+$settings->add(new admin_setting_configmultiselect('facetoface_session_roles', get_string('setting:sessionroles_caption', 'facetoface'), get_string('setting:sessionroles', 'facetoface'), array(), $choices));
 
-$returnurl = "view.php?f=$facetoface->id";
 
-// Handle deletions
-if ($d and $confirm) {
-    if (!confirm_sesskey()) {
-        print_error('confirmsesskeybad', 'error');
-    }
+$settings->add(new admin_setting_heading('facetoface_manageremail_header', get_string('manageremailheading', 'facetoface'), ''));
 
-    if (facetoface_delete_session($session)) {
-        add_to_log($course->id, 'facetoface', 'delete session', 'sessions.php?s='.$session->id, $facetoface->id, $cm->id);
-    }
-    else {
-        add_to_log($course->id, 'facetoface', 'delete session (FAILED)', 'sessions.php?s='.$session->id, $facetoface->id, $cm->id);
-        print_error('error:couldnotdeletesession', 'facetoface', $returnurl);
-    }
-    redirect($returnurl);
-}
+$settings->add(new admin_setting_configcheckbox('facetoface_addchangemanageremail', get_string('setting:addchangemanageremail_caption', 'facetoface'),get_string('setting:addchangemanageremail', 'facetoface'), 0));
 
-$customfields = facetoface_get_session_customfields();
+$settings->add(new admin_setting_configtext('facetoface_manageraddressformat', get_string('setting:manageraddressformat_caption', 'facetoface'),get_string('setting:manageraddressformat', 'facetoface'), get_string('setting:manageraddressformatdefault', 'facetoface'), PARAM_TEXT));
 
-$mform = new mod_facetoface_session_form(null, compact('id', 'f', 's', 'c', 'nbdays', 'customfields', 'course'));
-if ($mform->is_cancelled()) {
-    redirect($returnurl);
-}
+$settings->add(new admin_setting_configtext('facetoface_manageraddressformatreadable', get_string('setting:manageraddressformatreadable_caption', 'facetoface'),get_string('setting:manageraddressformatreadable', 'facetoface'), get_string('setting:manageraddressformatreadabledefault', 'facetoface'), PARAM_NOTAGS));
 
-if ($fromform = $mform->get_data()) { // Form submitted
 
-    if (empty($fromform->submitbutton)) {
-        print_error('error:unknownbuttonclicked', 'facetoface', $returnurl);
-    }
+$settings->add(new admin_setting_heading('facetoface_cost_header', get_string('costheading', 'facetoface'), ''));
 
-    // Pre-process fields
-    if (empty($fromform->allowoverbook)) {
-        $fromform->allowoverbook = 0;
-    }
-    if (empty($fromform->duration)) {
-        $fromform->duration = 0;
-    }
-    if (empty($fromform->normalcost)) {
-        $fromform->normalcost = 0;
-    }
-    if (empty($fromform->discountcost)) {
-        $fromform->discountcost = 0;
-    }
-    
-    //DK
-    if(empty($fromform->disablenewenrolldays))
-    {
-    	$fromform->disablenewenrolldays = 0;
-    }
-    if (empty($fromform->disableoption))
-    {
-    	$fromform->disableoption = 0;
-    }
-    
-    $sessiondates = array();
-    for ($i = 0; $i < $fromform->date_repeats; $i++) {
-        if (!empty($fromform->datedelete[$i])) {
-            continue; // skip this date
-        }
+$settings->add(new admin_setting_configcheckbox('facetoface_hidecost', get_string('setting:hidecost_caption', 'facetoface'),get_string('setting:hidecost', 'facetoface'), 0));
 
-        $timestartfield = "timestart[$i]";
-        $timefinishfield = "timefinish[$i]";
-        if (!empty($fromform->$timestartfield) and !empty($fromform->$timefinishfield)) {
-            $date = new stdClass();
-            $date->timestart = $fromform->$timestartfield;
-            $date->timefinish = $fromform->$timefinishfield;
-            $sessiondates[] = $date;
-        }
-    }
+$settings->add(new admin_setting_configcheckbox('facetoface_hidediscount', get_string('setting:hidediscount_caption', 'facetoface'),get_string('setting:hidediscount', 'facetoface'), 0));
 
-    //DK
-    $todb = new stdClass();
-    $todb->facetoface = $facetoface->id;
-    $todb->datetimeknown = $fromform->datetimeknown;
-    $todb->capacity = $fromform->capacity;
-    $todb->allowoverbook = $fromform->allowoverbook;
-    $todb->duration = $fromform->duration;
-    $todb->normalcost = $fromform->normalcost;
-    $todb->discountcost = $fromform->discountcost;
-//     $todb->details = trim($fromform->details['text']);
-    $todb->details = $fromform->details;
-    $todb->disablenewenrolldays = $fromform->disablenewenrolldays;
-    $todb->disableoption = $fromform->disableoption;
 
-    $sessionid = null;
-    $transaction = $DB->start_delegated_transaction();
+$settings->add(new admin_setting_heading('facetoface_icalendar_header', get_string('icalendarheading', 'facetoface'), ''));
 
-    $update = false;
-    if (!$c and $session != null) {
-        $update = true;
-        $sessionid = $session->id;
+$settings->add(new admin_setting_configcheckbox('facetoface_oneemailperday', get_string('setting:oneemailperday_caption', 'facetoface'),get_string('setting:oneemailperday', 'facetoface'), 0));
 
-        $todb->id = $session->id;
-        if (!facetoface_update_session($todb, $sessiondates)) {
-            $transaction->force_transaction_rollback();
-            add_to_log($course->id, 'facetoface', 'update session (FAILED)', "sessions.php?s=$session->id", $facetoface->id, $cm->id);
-            print_error('error:couldnotupdatesession', 'facetoface', $returnurl);
-        }
+$settings->add(new admin_setting_configcheckbox('facetoface_disableicalcancel', get_string('setting:disableicalcancel_caption', 'facetoface'),get_string('setting:disableicalcancel', 'facetoface'), 0));
 
-        // Remove old site-wide calendar entry
-        if (!facetoface_remove_session_from_site_calendar($session)) {
-            $transaction->force_transaction_rollback();
-            print_error('error:couldnotupdatecalendar', 'facetoface', $returnurl);
-        }
-    }
-    else {
-        if (!$sessionid = facetoface_add_session($todb, $sessiondates)) {
-            $transaction->force_transaction_rollback();
-            add_to_log($course->id, 'facetoface', 'add session (FAILED)', 'sessions.php?f='.$facetoface->id, $facetoface->id, $cm->id);
-            print_error('error:couldnotaddsession', 'facetoface', $returnurl);
-        }
-    }
 
-    foreach ($customfields as $field) {
-        $fieldname = "custom_$field->shortname";
-        if (!isset($fromform->$fieldname)) {
-            $fromform->$fieldname = ''; // need to be able to clear fields
-        }
+// List of existing custom fields
+$html = facetoface_list_of_customfields();
+$html .= html_writer::start_tag('p');
+$url = new moodle_url('/mod/facetoface/customfield.php', array('id' => 0));
+$html .= html_writer::link($url, get_string('addnewfieldlink', 'facetoface'));
+$html .= html_writer::end_tag('p');
 
-        if (!facetoface_save_customfield_value($field->id, $fromform->$fieldname, $sessionid, 'session')) {
-            $transaction->force_transaction_rollback();
-            print_error('error:couldnotsavecustomfield', 'facetoface', $returnurl);
-        }
-    }
+$settings->add(new admin_setting_heading('facetoface_customfields_header', get_string('customfieldsheading', 'facetoface'), $html));
 
-    // Save trainer roles
-    if (isset($fromform->trainerrole)) {
-        facetoface_update_trainers($sessionid, $fromform->trainerrole);
-    }
+// List of existing site notices
+$html = facetoface_list_of_sitenotices();
+$html .= html_writer::start_tag('p');
+$url = new moodle_url('/mod/facetoface/sitenotice.php', array('id' => 0));
+$html .= html_writer::link($url, get_string('addnewnoticelink', 'facetoface'));
+$html .= html_writer::end_tag('p');
 
-    // Retrieve record that was just inserted/updated
-    if (!$session = facetoface_get_session($sessionid)) {
-        $transaction->force_transaction_rollback();
-        print_error('error:couldnotfindsession', 'facetoface', $returnurl);
-    }
-
-    // Put the session in the site-wide calendar (needs customfields to be up to date)
-    if (!facetoface_add_session_to_site_calendar($session, $facetoface)) {
-        $transaction->force_transaction_rollback();
-        print_error('error:couldnotupdatecalendar', 'facetoface', $returnurl);
-    }
-
-    if ($update) {
-        add_to_log($course->id, 'facetoface', 'updated session', "sessions.php?s=$session->id", $facetoface->id, $cm->id);
-    }
-    else {
-        add_to_log($course->id, 'facetoface', 'added session', 'facetoface', 'sessions.php?f='.$facetoface->id, $facetoface->id, $cm->id);
-    }
-
-    $transaction->allow_commit();
-    redirect($returnurl);
-}
-elseif ($session != null) { // Edit mode
-    // Set values for the form //DK
-    $toform = new stdClass();
-    $toform->datetimeknown = (1 == $session->datetimeknown);
-    $toform->capacity = $session->capacity;
-    $toform->allowoverbook = $session->allowoverbook;
-    $toform->duration = $session->duration;
-    $toform->normalcost = $session->normalcost;
-    $toform->discountcost = $session->discountcost;
-    $toform->details = $session->details;
-    //DK
-    $toform->disableoption = $session->disableoption;
-    $toform->disablenewenrolldays = $session->disablenewenrolldays;
-    
-    if ($session->sessiondates) {
-        $i = 0;
-        foreach ($session->sessiondates as $date) {
-            $idfield = "sessiondateid[$i]";
-            $timestartfield = "timestart[$i]";
-            $timefinishfield = "timefinish[$i]";
-            $toform->$idfield = $date->id;
-            $toform->$timestartfield = $date->timestart;
-            $toform->$timefinishfield = $date->timefinish;
-            $i++;
-        }
-    }
-
-    foreach ($customfields as $field) {
-        $fieldname = "custom_$field->shortname";
-        $toform->$fieldname = facetoface_get_customfield_value($field, $session->id, 'session');
-    }
-
-    $mform->set_data($toform);
-}
-
-if ($c) {
-    $heading = get_string('copyingsession', 'facetoface', $facetoface->name);
-}
-else if ($d) {
-    $heading = get_string('deletingsession', 'facetoface', $facetoface->name);
-}
-else if ($id or $f) {
-    $heading = get_string('addingsession', 'facetoface', $facetoface->name);
-}
-else {
-    $heading = get_string('editingsession', 'facetoface', $facetoface->name);
-}
-
-$pagetitle = format_string($facetoface->name);
-
-$PAGE->set_cm($cm);
-$PAGE->set_url('/mod/facetoface/sessions.php', array('f' => $f));
-$PAGE->set_title($pagetitle);
-$PAGE->set_heading($course->fullname);
-
-echo $OUTPUT->header();
-
-echo $OUTPUT->box_start();
-echo $OUTPUT->heading($heading);
-
-if (!empty($errorstr)) {
-    echo $OUTPUT->container(html_writer::tag('span', $errorstr, array('class' => 'errorstring')), array('class' => 'notifyproblem'));
-}
-
-if ($d) {
-    $viewattendees = has_capability('mod/facetoface:viewattendees', $context);
-    facetoface_print_session($session, $viewattendees);
-    $optionsyes = array('sesskey' => sesskey(), 's' => $session->id, 'd' => 1, 'confirm' => 1);
-    echo $OUTPUT->confirm(get_string('deletesessionconfirm', 'facetoface', format_string($facetoface->name)),
-        new moodle_url('sessions.php', $optionsyes),
-        new moodle_url($returnurl));
-}
-else {
-    $mform->display();
-}
-
-echo $OUTPUT->box_end();
-echo $OUTPUT->footer($course);
+$settings->add(new admin_setting_heading('facetoface_sitenotices_header', get_string('sitenoticesheading', 'facetoface'), $html));
